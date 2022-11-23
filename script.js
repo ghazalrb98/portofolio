5167b28aa534e246151bc1d768318e5e5f4d704d
ghazalrb98 <ghazalrb.98@gmail.com>
2022-11-23 09:54:31 +0100

    Add config file of constant position ratios, Initial script of navbar

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
+import config from "./config.json" assert { type: "json" };
+
 const header = document.getElementById("header");
 const profilePic = document.getElementById("profile-pic");
 const causeScrollDiv = document.getElementById("cause-scroll-div");
+const navBar = document.getElementById("nav-bar");
 
 function setHeight(element, height, unit) {
   element.style.height = `${height}${unit}`;
 }
 
-// window.addEventListener("resize", function () {
-//   setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
-// });
+function setBottom(element, bottom, unit) {
+  element.style.bottom = `${bottom}${unit}`;
+}
+
+window.addEventListener("resize", function () {
+  setHeight(
+    causeScrollDiv,
+    document.documentElement.clientHeight +
+      config.headerAnimation.causeScrollDivExtraHeight,
+    "px"
+  );
+  setHeight(
+    navBar,
+    header.clientWidth * config.headerAnimation.NavHeightWidthRatio,
+    "px"
+  );
+  setBottom(
+    navBar,
+    causeScrollDiv.clientHeight * config.headerAnimation.NavBottomRatio,
+    "px"
+  );
+});
 
 document.addEventListener("scroll", function () {
   const scrollTopValue = Math.floor(document.documentElement.scrollTop / 20);
   profilePic.setAttribute("src", `/images/header_step${scrollTopValue}.svg`);
 });
 
-// setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
+setHeight(
+  causeScrollDiv,
+  document.documentElement.clientHeight +
+    config.headerAnimation.causeScrollDivExtraHeight,
+  "px"
+);
+setHeight(
+  navBar,
+  header.clientWidth * config.headerAnimation.NavHeightWidthRatio,
+  "px"
+);
+setBottom(
+  navBar,
+  causeScrollDiv.clientHeight * config.headerAnimation.NavBottomRatio,
+  "px"
+);
