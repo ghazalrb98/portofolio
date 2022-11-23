4d77f249c894c5a1d29fae2947135700a4a31d83
ghazalrb98 <ghazalrb.98@gmail.com>
2022-11-23 08:23:08 +0100

    Add navbar animation

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function setHeight(element, height, unit) {
   element.style.height = `${height}${unit}`;
 }
 
-window.addEventListener("resize", function () {
-  setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
-});
+// window.addEventListener("resize", function () {
+//   setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
+// });
 
 document.addEventListener("scroll", function () {
   const scrollTopValue = Math.floor(document.documentElement.scrollTop / 20);
   profilePic.setAttribute("src", `/images/header_step${scrollTopValue}.svg`);
 });
 
-setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
+// setHeight(causeScrollDiv, document.documentElement.clientHeight + 820, "px");
